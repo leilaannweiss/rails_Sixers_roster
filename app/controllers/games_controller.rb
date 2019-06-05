545fd7c2702b22e01e3696928652348c1baee4cb
Leila Weiss <leila.weiss@powerhrg.com>
2019-06-05 12:32:11 -0400

    index page

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+    @game = Game.new
+  end
+end
